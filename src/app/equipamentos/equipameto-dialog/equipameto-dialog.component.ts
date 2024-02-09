1175b1c0b4c8a0c6cce56d4d555b18101cd11a37
matheus <matheus.henrique@elabbora.com.br>
2024-02-08 22:01:33 -0300

    FormEqp: Corrige form de equipamento

--- src/app/equipamentos/equipameto-dialog/equipameto-dialog.component.ts
+++ src/app/equipamentos/equipameto-dialog/equipameto-dialog.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Equipamento } from 'app/model/equipamento';
+import { EquipamentoService } from 'app/service/equipamento.service';
 
 @Component({
   selector: 'app-equipameto-dialog',
@@ -8,8 +11,51 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 })
 export class EquipametoDialogComponent {
 
-  constructor(    private _form: FormBuilder,
-    ) { }
+  form: FormGroup;
+  equioamentoList: Equipamento[];
+
+  isEdit: boolean;
+
+
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: {equipamento: Equipamento},   
+     private _form: FormBuilder,
+     private servic: EquipamentoService
+    ) { 
+      this.isEdit= Boolean(this.data.equipamento.id);
+    }
+
+  ngOnInit(): void {
+ 
+    this.form = this._form.group({
+      id: [this.data.equipamento.id],
+      nome: [this.data.equipamento.nome_equipamento, Validators.maxLength(100)],
+      status: [this.data.equipamento.status, Validators.maxLength(100)],
+      
+    });
+    }
+    onSubmit(): void {
+      const equipamento: Equipamento = {
+          id: this.form.get('id').value,
+          nome_equipamento: this.form.get('nome').value,
+          status: this.form.get('status').value
+      };
+  
+      // Enviar os dados do formulário
+      this.servic.addEquipamento(equipamento).subscribe(
+          response => {
+              console.log('Dados enviados com sucesso:', response);
+              // Reinicializar o formulário ou outras ações
+          },
+          error => {
+              console.error('Erro ao enviar dados:', error);
+              // Tratar o erro, se necessário
+          }
+      );
+  }
+  
+
 
 
 }
