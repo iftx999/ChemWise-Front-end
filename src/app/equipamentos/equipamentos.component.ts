8e95e84b2f4c8d638aca4244bd4f9b3d3ec2e00e
matheus <matheus.henrique@elabbora.com.br>
2024-02-11 12:41:07 -0300

    Corrige botão de alterar

--- src/app/equipamentos/equipamentos.component.ts
+++ src/app/equipamentos/equipamentos.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { EquipametoDialogComponent } from './equipameto-dialog/equipameto-dialog.component';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { Router } from '@angular/router';
 import { EquipamentoService } from 'app/service/equipamento.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 import { Equipamento } from 'app/model/equipamento';
-
 
 @Component({
   selector: 'app-equipamentos',
@@ -17,72 +15,65 @@ export class EquipamentosComponent implements OnInit {
 
   equipamentoList: Equipamento[];
   equipamentoDataSource: MatTableDataSource<Equipamento>;
-  equipamentoColuns: string[] = ['nome_equipamento', 'status'];
+  equipamentoColuns: string[] = ['nome_equipamento', 'status', 'actions'];
 
   @ViewChild(MatSort) sort: MatSort;
 
-
-
-  constructor(    private _dialog: MatDialog,
-    public eqpService: EquipamentoService,
-
-
-
-    ) { }
+  constructor(
+    private dialog: MatDialog,
+    private equipamentoService: EquipamentoService
+  ) { }
 
   ngOnInit(): void {
     this.listarEquipamentos();
   }
-   
-  
+
   listarEquipamentos(): void {
-  this.eqpService.getEquipamentos().subscribe((data) => { 
-  this.equipamentoList = data; // Atualize a lista de equipamentos
-      this.equipamentoDataSource = new MatTableDataSource(this.equipamentoList); // Atualize o dataSource da tabela
+    this.equipamentoService.getEquipamentos().subscribe(data => {
+      console.log(data);
+      this.equipamentoList = data;
+      this.equipamentoDataSource = new MatTableDataSource(this.equipamentoList);
+    });
+  }
+
+  salvarOuAtualizarEquipamento(equipamento: Equipamento): void {
+    if (!equipamento.id) {
+      this.salvarEquipamento(equipamento);
+    } else {
+      this.atualizarEquipamento(equipamento);
     }
-    );
-  } 
+  }
 
-  
-  //salvar equipamento
   salvarEquipamento(equipamento: Equipamento): void {
-    this.eqpService.addEquipamento(equipamento).subscribe((data) => {
+    this.equipamentoService.addEquipamento(equipamento).subscribe(() => {
       this.listarEquipamentos();
     });
   }
 
-//atualizar equipamento
-  atualizaCanal(id: number, equipamento: Equipamento): void {
-    this.eqpService.updateEquipamento(id, equipamento).subscribe((data) => {
+  atualizarEquipamento(equipamento: Equipamento): void {
+    this.equipamentoService.updateEquipamento(equipamento.id, equipamento).subscribe(() => {
       this.listarEquipamentos();
     });
   }
-
-
-
 
   openDialog(row?: Equipamento): void {
+    console.log('Abrindo diálogo para equipamento com ID:', row ? row.id : 'novo');
+    console.log(row ? row : new Equipamento());
     const dialogConfig = new MatDialogConfig();
     dialogConfig.autoFocus = true;
     dialogConfig.width = "40%";
     dialogConfig.minWidth = "380px";
-
     dialogConfig.data = {
       equipamento: row ? row : new Equipamento()
     };
-
-    let dialogRef = this._dialog.open(EquipametoDialogComponent, dialogConfig);
-    dialogRef.afterClosed()
-      .subscribe(result => {
-        if (!result) { return; }
-
-        if (!result.id) {
-          this.salvarEquipamento(result);
-        } else {
-          this.atualizaCanal(result.id, result);
-          
-        }
-      });
+  
+    const dialogRef = this.dialog.open(EquipametoDialogComponent, dialogConfig);
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.salvarOuAtualizarEquipamento(result);
+      }
+    });
   }
+  
 
 }
--- src/app/equipamentos/equipameto-dialog/equipameto-dialog.component.ts
+++ src/app/equipamentos/equipameto-dialog/equipameto-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Equipamento } from 'app/model/equipamento';
 import { EquipamentoService } from 'app/service/equipamento.service';
@@ -12,50 +12,58 @@ import { EquipamentoService } from 'app/service/equipamento.service';
 export class EquipametoDialogComponent {
 
   form: FormGroup;
-  equioamentoList: Equipamento[];
-
   isEdit: boolean;
 
-
-
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: {equipamento: Equipamento},   
-     private _form: FormBuilder,
-     private servic: EquipamentoService
-    ) { 
-      this.isEdit= Boolean(this.data.equipamento.id);
-    }
+    @Inject(MAT_DIALOG_DATA) public data: { equipamento: Equipamento },
+    private _formBuilder: FormBuilder,
+    private equipamentoService: EquipamentoService
+  ) {
+    this.isEdit = !!this.data.equipamento.id; // Verifica se está editando
+    this.createForm();
+  }
 
-  ngOnInit(): void {
- 
-    this.form = this._form.group({
+  createForm(): void {
+    console.log('ID do equipamento recebido:', this.data.equipamento.id);
+    this.form = this._formBuilder.group({
       id: [this.data.equipamento.id],
       nome: [this.data.equipamento.nome_equipamento, Validators.maxLength(100)],
-      status: [this.data.equipamento.status, Validators.maxLength(100)],
-      
+      status: [this.data.equipamento.status, Validators.maxLength(100)]
     });
-    }
-    onSubmit(): void {
-      const equipamento: Equipamento = {
-          id: this.form.get('id').value,
-          nome_equipamento: this.form.get('nome').value,
-          status: this.form.get('status').value
-      };
-  
-      // Enviar os dados do formulário
-      this.servic.addEquipamento(equipamento).subscribe(
-          response => {
-              console.log('Dados enviados com sucesso:', response);
-              // Reinicializar o formulário ou outras ações
-          },
-          error => {
-              console.error('Erro ao enviar dados:', error);
-              // Tratar o erro, se necessário
-          }
-      );
   }
   
 
+  onSubmit(): void {
+    const equipamento: Equipamento = {
+      id: this.form.get('id').value,
+      nome_equipamento: this.form.get('nome').value,
+      status: this.form.get('status').value
+    };
 
-
+    if (this.isEdit) {
+      // Atualizar equipamento existente
+      this.equipamentoService.updateEquipamento(this.data.equipamento.id, equipamento).subscribe(
+        response => {
+          console.log('Equipamento atualizado com sucesso:', response);
+          // Outras ações, se necessário
+        },
+        error => {
+          console.error('Erro ao atualizar equipamento:', error);
+          // Tratar o erro, se necessário
+        }
+      );
+    } else {
+      // Adicionar novo equipamento
+      this.equipamentoService.addEquipamento(equipamento).subscribe(
+        response => {
+          console.log('Equipamento adicionado com sucesso:', response);
+          // Outras ações, se necessário
+        },
+        error => {
+          console.error('Erro ao adicionar equipamento:', error);
+          // Tratar o erro, se necessário
+        }
+      );
+    }
+  }
 }
